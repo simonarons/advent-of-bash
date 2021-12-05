f238c878439cda21d73038ae4eb3206f14e4a29b
Simon Arons <simon@arons.se>
2021-12-05 13:35:08 +0000

    Oh my, embarrassing double-loop mistake.

--- 2021/day03/day03-01.sh
+++ 2021/day03/day03-01.sh
@@ -3,6 +3,5 @@
 [[ -t 0 ]] && { [[ -f $1 ]] && input=$(<"$1") || { echo "error: no input"; exit 1; }; } || input=$(</dev/stdin)
 
 len=$(wc -l <<< "$input")
-for i in {1..12}; do (( $(cut -b${i} <<< "$input" | grep -c 0) > len/2 )) && g=${g}0 || g=${g}1; done
-for i in {1..12}; do (( $(cut -b${i} <<< "$input" | grep -c 0) < len/2 )) && e=${e}0 || e=${e}1; done
+for i in {1..12}; do (( $(cut -b${i} <<< "$input" | grep -c 0) > len/2 )) && g=${g}0 e=${e}1 || g=${g}1 e=${e}0; done
 echo $(( 2#$g * 2#$e ))
