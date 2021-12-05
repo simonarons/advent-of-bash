ae5a343b29bb31410ade8ace76267e67986dfbe0
Simon Arons <simon@arons.se>
2021-12-05 13:32:40 +0000

    Day 03 reporting in.

---
+++ 2021/day03/day03-01.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Allow input from stdin or file, 'cause what more could one want?
+[[ -t 0 ]] && { [[ -f $1 ]] && input=$(<"$1") || { echo "error: no input"; exit 1; }; } || input=$(</dev/stdin)
+
+len=$(wc -l <<< "$input")
+for i in {1..12}; do (( $(cut -b${i} <<< "$input" | grep -c 0) > len/2 )) && g=${g}0 || g=${g}1; done
+for i in {1..12}; do (( $(cut -b${i} <<< "$input" | grep -c 0) < len/2 )) && e=${e}0 || e=${e}1; done
+echo $(( 2#$g * 2#$e ))
